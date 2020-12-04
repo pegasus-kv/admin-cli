ba78a9de04c578725515391dbdbf26de0c6c7705
JiaShuo <js982986555@live.com>
2020-12-04 20:01:26 +0800

    init

--- executor/use_table.go
+++ executor/use_table.go
@@ -5,24 +5,21 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/XiaoMi/pegasus-go-client/idl/admin"
+	"github.com/XiaoMi/pegasus-go-client/idl/base"
 )
 
 func UseTable(client *Client, table string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
-	resp, err := client.Meta.ListApps(ctx, &admin.ListAppsRequest{
-		Status: admin.AppStatus_AS_AVAILABLE,
-	})
+	resp, err := client.Meta.QueryConfig(ctx, table)
 	if err != nil {
 		return err
 	}
 
-	for _, info := range resp.Infos {
-		if info.AppName == table {
-			return nil
-		}
+	if resp.GetErr().Errno != base.ERR_OK.String() {
+		return fmt.Errorf("Use Table(%s) failed: %s", table, resp.GetErr().String())
 	}
-	return fmt.Errorf(fmt.Sprintf("Table \"%s\" doesn't exist!", table))
+
+	return nil
 }
