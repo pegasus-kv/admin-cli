b0d9af7a066f8c80ddb06685988f4b78a93deafa
jiashuo <js982986555@live.com>
2021-06-03 09:51:04 +0800

    two:imp framework

--- executor/disk_balance.go
+++ executor/disk_balance.go
@@ -25,8 +25,10 @@ import (
 	"strings"
 	"time"
 
+	"github.com/XiaoMi/pegasus-go-client/idl/admin"
 	"github.com/XiaoMi/pegasus-go-client/idl/radmin"
 	"github.com/XiaoMi/pegasus-go-client/session"
+	adminClient "github.com/pegasus-kv/admin-cli/client"
 	"github.com/pegasus-kv/admin-cli/util"
 )
 
@@ -149,17 +151,72 @@ func (m *MigrateAction) toString() string {
 	return fmt.Sprintf("node=%s, replica=%s, %s=>%s", m.node, m.replica.Gpid, m.from, m.to)
 }
 
-// TODO(jiashuo1): next pr
 func changeDiskCleanerInterval(client *Client, replicaServer string, cleanInterval int64) error {
+	fmt.Printf("set gc_disk_migration_origin_replica_interval_seconds = %ds ", cleanInterval)
+	err := ConfigCommand(client, session.NodeTypeReplica, replicaServer,
+		"gc_disk_migration_origin_replica_interval_seconds", "set", cleanInterval)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-// TODO(jiashuo1):next pr
 func getNextMigrateAction(client *Client, replicaServer string, minSize int64) (*MigrateAction, error) {
+	disks, totalUsage, totalCapacity, err := queryDiskCapacityInfo(client, replicaServer)
+	if err != nil {
+		return nil, err
+	}
+	diskMigrateInfo, err := getMigrateDiskInfo(client, replicaServer, disks, totalUsage, totalCapacity)
+	if err != nil {
+		return nil, err
+	}
+
+	migrateAction, err := computeMigrateAction(diskMigrateInfo, minSize)
+	if err != nil {
+		return nil, err
+	}
+	return migrateAction, nil
+}
+
+func forceAssignReplicaToSecondary(client *Client, replicaServer string, gpid string) error {
+	fmt.Printf("WARNING: the select replica is not secondary, will force assign it secondary\n")
+	_, err := client.Meta.MetaControl(admin.MetaFunctionLevel_fl_steady)
+	if err != nil {
+		return err
+	}
+	secondaryNode, err := getReplicaSecondaryNode(client, gpid)
+	if err != nil {
+		return err
+	}
+	replica, err := util.Str2Gpid(gpid)
+	if err != nil {
+		return err
+	}
+	err = client.Meta.Balance(replica, adminClient.BalanceMovePri,
+		util.NewNodeFromTCPAddr(replicaServer, session.NodeTypeReplica), secondaryNode)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// todo(jiashuo1) next pr
+func getReplicaSecondaryNode(client *Client, gpid string) (*util.PegasusNode, error) {
 	return nil, nil
 }
 
-// TODO(jiashuo1):next pr
-func forceAssignReplicaToSecondary(client *Client, replicaServer string, gpid string) error {
-	return nil
+// todo(jiashuo1) next pr
+func queryDiskCapacityInfo(client *Client, replicaServer string) ([]DiskCapacityStruct, int64, int64, error) {
+	return nil, 0, 0, nil
+}
+
+// todo(jiashuo1) next pr
+func getMigrateDiskInfo(client *Client, replicaServer string, disks []DiskCapacityStruct,
+	totalUsage int64, totalCapacity int64) (*MigrateDisk, error) {
+	return nil, nil
+}
+
+// todo(jiashuo1) next pr
+func computeMigrateAction(migrate *MigrateDisk, minSize int64) (*MigrateAction, error) {
+	return nil, nil
 }
