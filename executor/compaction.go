df972ddbd16ef81890d139989135d07ccf2ca886
levy <zlw5307@163.com>
2021-09-14 16:32:43 +0800

    fix: fix bug in user specified compaction

--- executor/compaction.go
+++ executor/compaction.go
@@ -138,7 +138,7 @@ func generateRules(params *CompactionParams) ([]compactionRule, error) {
 	return res, nil
 }
 
-var ruleTypeMapping = map[string]string{
+var matchTypeMapping = map[string]string{
 	"anywhere": "SMT_MATCH_ANYWHERE",
 	"prefix":   "SMT_MATCH_PREFIX",
 	"postfix":  "SMT_MATCH_POSTFIX",
@@ -148,7 +148,7 @@ func generateKeyRule(ruleType string, pattern string, match string) (*compaction
 	var params keyRuleParams
 	params.Pattern = pattern
 	ok := false
-	if params.MatchType, ok = ruleTypeMapping[ruleType]; !ok {
+	if params.MatchType, ok = matchTypeMapping[match]; !ok {
 		return nil, fmt.Errorf("invalid match type {%s}", match)
 	}
 
