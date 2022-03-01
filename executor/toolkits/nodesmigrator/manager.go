dfd8ff6169efae90c1e678446aa918b301e8298b
jiashuo <js982986555@live.com>
2022-03-01 13:42:02 +0800

    update

--- executor/toolkits/nodesmigrator/manager.go
+++ executor/toolkits/nodesmigrator/manager.go
@@ -75,7 +75,7 @@ func MigrateAllReplicaToNodes(client *executor.Client, from []string, to []strin
 		}
 
 		if !GlobalBatchTarget {
-			if runTargetCount == targetCount {
+			if runTargetCount / targetCount >= 1 {
 				balanceFactor++
 			}
 		} else {
